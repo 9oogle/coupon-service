833bc04b65f2392ea01c9e54698bd1768dc82aa9
hyew0nn <hyewon-0607@naver.com>
2026-05-18 02:17:49 +0900

    style: spotless 스타일 적용

--- src/test/java/com/goggles/coupon/CouponApplicationTests.java
+++ src/test/java/com/goggles/coupon/CouponApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CouponApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
